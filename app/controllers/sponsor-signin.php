353b91597757ec81a721aea77bd7c427e44132e1
2023cs158-sudo <2023cs158@stu.ucsc.cmb.ac.lk>
2025-08-20 10:23:01 +0530

    Add complete HTML structure for the sponsor sign-in page

---
+++ app/controllers/sponsor-signin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp</title>
+    <link rel="stylesheet" href="index.styles.css">
+</head>
+
+<body>
+    <form action=""> 
+        <div class="box">
+            <div class="container">
+            <h2>Sponsor</h2>
+            </div>
+            <div class = "form-row">
+                <div class="input-group">
+                        <label for="name">Name</label>
+                        <input type="text" placeholder="Enter your name">
+                </div>  
+                <div class="input-group">
+                        <label for="email">Email</label>
+                        <input type="email" placeholder="Enter your email">
+                </div> 
+            </div>
+            <div class="form-row">
+                <div class="input-group">
+                        <label for="password">Password</label>
+                        <input type="password" placeholder="Enter your password">
+                </div>
+                <div class="input-group">
+                        <label for="confirm-password">Re-enter password</label>
+                        <input type="password" placeholder="Re-enter password">
+                </div>
+            </div>
+            
+            <div class="center-submit">
+                <input type="submit" value="Sign Up">
+            </div>
+            
+                    
+        </div>
+    </form>
+</body>
+
+</html>
