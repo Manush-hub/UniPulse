00464b2fadf874edadd2a9c6dc4f715638faa531
2023cs158-sudo <2023cs158@stu.ucsc.cmb.ac.lk>
2025-09-01 15:54:01 +0530

    Add initial implementation of Sponsor Dashboard controller

---
+++ app/controllers/Sponsor/Dashboard.php
@@ -0,0 +1,23 @@
+<?php
+
+class Dashboard extends Controller{
+
+    public function __construct() {
+        parent::__construct();
+        // Ensure only admins can access this controller
+        $this->requireRole('Sponsor');
+    }
+
+    public function index($a = '', $b = '' , $c = ''){
+        // Sponsor-specific dashboard
+        $data = [
+            'title' => 'Sponsor Dashboard',
+            'user_role' => $this->userRole
+        ];
+        $this->view('dashboard', $data);
+    } 
+    
+    public function users($a = '', $b = '' , $c = ''){
+        // User management
+        $this->view('users');
+    }
