c990aa9acdec9d5fa105de9b166993432f802c96
2023cs200-sys <2023cs200@stu.ucsc.cmb.ac.lk>
2025-09-04 20:49:58 +0530

    no changes

--- app/views/Publisher/dashboard.view.php
+++ app/views/Publisher/dashboard.view.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UniPulse - Event Organizer Dashboard</title>
     <link rel="stylesheet" href="<?php echo $controller->loadCSS('dashboard-style.css'); ?>">
+
 </head>
 
 <body>
