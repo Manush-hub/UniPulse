52738ce34d283b01dca819a94d92216e3caaecd7
R P M A Perera <2023cs144@stu.ucsc.cmb.ac.lk>
2025-09-08 13:38:42 +0530

    Remove obsolete setup verification, test forms, and authentication test files to streamline the public directory and improve code maintainability.

--- public/test_password.php
+++
@@ -1,9 +0,0 @@
-<?php
-$password = '1142119112';
-$hash = '$2y$10$cxqC0tMkgx3I244jjv8Sk.qwXTU4mncK9tGXABlS.2tYH8.kZ.qgC';
-
-echo "Testing password hash verification:" . PHP_EOL;
-echo "Password: " . $password . PHP_EOL;
-echo "Hash: " . $hash . PHP_EOL;
-echo "Verification: " . (password_verify($password, $hash) ? 'SUCCESS' : 'FAILED') . PHP_EOL;
-?>
